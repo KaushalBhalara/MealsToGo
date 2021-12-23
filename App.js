dbbe7c9bd36c75a61bcdefa6d9c43e570ffc3872
Kaushal Bhalara <kaushal@webmynesystems.com>
2021-12-23 11:20:59 +0530

    Font Setup.

--- App.js
+++ App.js
@@ -12,9 +12,26 @@ import { ResturantScreen } from "./src/features/resturants/screen/resturantscree
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/globaldata/theme";
 
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
 const isAndroid = Platform.OS === "android";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!useOswald || !useLato) {
+    return null;
+  }
   return (
     <>
       <ThemeProvider theme={theme}>
