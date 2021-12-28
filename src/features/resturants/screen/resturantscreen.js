b6b35bd3b60e45d27c59e6450ab7a7a28b0157fc
Kaushal Bhalara <kaushal@webmynesystems.com>
2021-12-28 17:35:31 +0530

    FlastList Added Static

--- src/features/resturants/screen/resturantscreen.js
+++ src/features/resturants/screen/resturantscreen.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { SafeAreaView, View, Text, StyleSheet } from "react-native";
+import { SafeAreaView, View, Text, StyleSheet, FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { ResturantInfoCardComponent } from "../component/resturantinfocard.component";
 import styled from "styled-components";
+import { Spacer } from "../../../components/spacer/spacercomponent";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]}; ;
@@ -22,9 +23,31 @@ export const ResturantScreen = () => {
       <SearchContainer>
         <Searchbar></Searchbar>
       </SearchContainer>
-      <ResturantListContainer>
-        <ResturantInfoCardComponent />
-      </ResturantListContainer>
+      <FlatList
+        data={[
+          { name: 1 },
+          { name: 2 },
+          { name: 3 },
+          { name: 4 },
+          { name: 5 },
+          { name: 6 },
+          { name: 7 },
+          { name: 8 },
+          { name: 9 },
+          { name: 10 },
+          { name: 11 },
+          { name: 12 },
+          { name: 13 },
+          { name: 14 },
+        ]}
+        renderItem={() => (
+          <Spacer position="bottom" size="large">
+            <ResturantInfoCardComponent />
+          </Spacer>
+        )}
+        keyExtractor={(item) => item.name}
+        contentContainerStyle={{ padding: 16 }}
+      />
     </SafeArea>
   );
 };
