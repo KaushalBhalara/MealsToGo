7d9198f08b870f2d6329a1bf412d9eaf5b40d3f9
Kaushal Bhalara <kaushal@webmynesystems.com>
2022-01-06 16:39:28 +0530

    Callout & Press

---
+++ src/features/map/component/mapcallout.component.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components";
+import { CompactResturantCard } from "../../resturants/component/compact.resturantcard.component";
+
+const MyText = styled.Text``;
+
+export const MapCallout = ({ resturant }) => {
+  return <CompactResturantCard resturant={resturant}></CompactResturantCard>;
+};
