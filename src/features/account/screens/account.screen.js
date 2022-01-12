0642291a324065fc6476bdae7da14a763f535938
Kaushal Bhalara <kaushal@webmynesystems.com>
2022-01-12 14:24:21 +0530

    Login Screen Set up

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { Spacer } from "../../../components/spacer/spacercomponent";
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+  AuthButton,
+} from "../component/account.style";
 
-import { AccountBackground, AccountCover } from "../component/account.style";
-
-export const AccountScreen = () => {
+export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AccountContainer>
+        <AuthButton
+          icon="lock-open-outline"
+          mode="contained"
+          onPress={() => navigation.navigate("Login")}
+        >
+          Login
+        </AuthButton>
+        <Spacer size="large" />
+        <AuthButton
+          icon="lock-open-outline"
+          mode="contained"
+          onPress={() => navigation.navigate("Sign up")}
+        >
+          Register
+        </AuthButton>
+      </AccountContainer>
     </AccountBackground>
   );
 };
