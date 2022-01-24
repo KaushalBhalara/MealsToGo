d1e75e29ad9722240cb14d8e671821e9428f700c
Kaushal Bhalara <kaushal@webmynesystems.com>
2022-01-24 17:33:26 +0530

    Setting Screen Update

--- src/features/settings/screens/setting.screen.js
+++ src/features/settings/screens/setting.screen.js
@@ -1,20 +1,36 @@
 import React, { useContext } from "react";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
-import { Text, Button } from "react-native";
-import { List } from "react-native-paper";
+import { List, Avatar } from "react-native-paper";
+import styled from "styled-components";
+import { Spacer } from "../../../components/spacer/spacercomponent";
+import { Text } from "../../../components/typography/textcomponent";
 
+const AvtarContainer = styled.View`
+  align-items: center;
+`;
+
+const SettingItem = styled(List.Item)`
+  padding: ${(props) => props.theme.space[2]};
+`;
 export const SettingsScreen = () => {
-  const { onLogout } = useContext(AuthenticationContext);
+  const { onLogout, user } = useContext(AuthenticationContext);
   return (
     <>
+      <AvtarContainer>
+        <Spacer position="top" size="large" />
+        <Avatar.Icon size={160} icon="human" backgroundColor="#2182BD" />
+        <Spacer position="top" size="large">
+          <Text variant="label">{user.email}</Text>
+        </Spacer>
+      </AvtarContainer>
       <List.Section>
-        <List.Item
+        <SettingItem
           style={{ padding: 16 }}
           title="Favourites"
           description="View your favourites"
           left={(props) => <List.Icon {...props} color="black" icon="heart" />}
         />
-        <List.Item
+        <SettingItem
           style={{ padding: 16 }}
           title="Logout"
           left={(props) => <List.Icon {...props} color="black" icon="door" />}
