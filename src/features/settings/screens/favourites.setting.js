8c83f956980914361b947c65ed12cb557d941a2e
Kaushal Bhalara <kaushal@webmynesystems.com>
2022-01-25 11:34:10 +0530

    Favourites Screen done

--- src/features/settings/screens/favourites.setting.js
+++ src/features/settings/screens/favourites.setting.js
@@ -1,3 +1,47 @@
-import React from "react";
+import React, { useContext } from "react";
+import { FlatList, TouchableOpacity } from "react-native";
+import { FavouritesContext } from "../../../services/favourites/favourites.contect";
+import styled from "styled-components";
+import { Spacer } from "../../../components/spacer/spacercomponent";
+import { ResturantInfoCardComponent } from "../../resturants/component/resturantinfocard.component";
+import { Text } from "../../../components/typography/textcomponent";
 
-export const FavouritesScreen = () => null;
+const ResturantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
+
+const NoFavouritesArea = styled.View`
+  align-items: center;
+  justify-content: center;
+`;
+
+export const FavouritesScreen = ({ navigation }) => {
+  const { favourites } = useContext(FavouritesContext);
+  return favourites.length ? (
+    <ResturantList
+      data={favourites}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate("ResturantDetail", {
+              resturant: item,
+            })
+          }
+        >
+          <Spacer position="bottom" size="large">
+            <ResturantInfoCardComponent resturant={item} />
+          </Spacer>
+        </TouchableOpacity>
+      )}
+      keyExtractor={(item) => item.name}
+    />
+  ) : (
+    <>
+      <NoFavouritesArea>
+        <Text variant="label">No Favourites</Text>
+      </NoFavouritesArea>
+    </>
+  );
+};
